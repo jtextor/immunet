360a8bad05e5d7f8e3ffee1d43f764a9c5f5297f
Evgenia Martynova <evg.martynova@gmail.com>
2023-06-23 13:41:35 +0200

    Add better visualisation of prediction

--- immunet/inference.py
+++ immunet/inference.py
@@ -1,5 +1,4 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="2"
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
 import warnings
 warnings.filterwarnings("ignore")
@@ -16,6 +15,28 @@ import argparse
 from PIL import Image
 from pathlib import Path
 from models import model_for_inference
+
+# The colors used for the prediction visualisation
+# Change according to the number of channels in your panel and preferred colors
+panel_colors = [
+    [255, 0, 0],
+    [0, 255, 0],
+    [255, 0, 255],
+    [255, 255, 0],
+    [0, 255, 255]
+]
+
+
+def mix(colors, fields):
+    im = np.zeros(fields[0].shape + (3,))
+    mxf = np.zeros(fields[0].shape)
+    for i in range(len(fields)):
+        exm = fields[i] > mxf
+        exmf = fields[i][exm]
+        for j in range(3):
+            im[exm, j] = exmf * colors[i][j]
+        mxf[exm] = exmf
+    return im
 
 
 def get_input(tile_path, in_channels_num):
@@ -85,16 +106,28 @@ def save_as_png(image_array, target_path):
     image.save(target_path, "PNG")
 
 
+def draw_cell_coordinates(image, coordinates, vis_radius):
+
+    for coordinate in coordinates:
+        iy, ix = int(round(coordinate[0])), int(round(coordinate[1]))
+        rr, cc = draw.circle(iy, ix, vis_radius, shape=image.shape)
+        image[rr, cc, :] = 255
+        rr, cc = draw.circle_perimeter(iy, ix, vis_radius, shape=image.shape)
+        image[rr, cc, :] = 0
+
+
 def demo_inference(tile_path,
                    model_path,
                    output_path,
+                   display_image_path=None,
                    log_threshold=0.07,
                    min_log_std=3,
                    max_log_std=5,
-                   dist_scalar=50):
+                   dist_scalar=50,
+                   vis_radius=2):
     output_path.mkdir(exist_ok=True)
 
-    model = tf.keras.models.load_model(str(model_path), custom_objects={"pool": tf.nn.pool, "pad": tf.pad}, compile=False) 
+    model = tf.keras.models.load_model(str(model_path), custom_objects={"pool": tf.nn.pool, "pad": tf.pad}, compile=False)
 
     in_channels_num = model.layers[0].input_shape[0][3]
     input = get_input(tile_path, in_channels_num)
@@ -108,23 +141,30 @@ def demo_inference(tile_path,
         subregion = crop_image_center(input)
         dist_map, pheno_map = predict(subregion, model, dist_scalar)
 
-    # Output 1:
-    # Make PNG visualization of distance prediction and all pseudochannels
-    # Save distance map prediction as image
-    save_as_png(dist_map, output_path / "distance-prediction.png")
-
-    # Save phenotype maps prediction as images
-    for i in range(pheno_map.shape[2]):
-        channel = scale_image(pheno_map[:, :, i], 255)
-        save_as_png(channel, output_path / "pseudochannel-{}.png".format(i + 1))
-
-    # Output 2:
-    # Use Laplacian-Of-Gaussian filter to detect cell locations and output
-    # locations and phenotypes
     cell_centers = [(int(x[0]), int(x[1]))
                     for x in blob_log(dist_map, min_sigma=min_log_std, max_sigma=max_log_std, threshold=log_threshold)]
-
     print("{} lymphocytes has been detected".format(len(cell_centers)))
+
+    # Output 1:
+    # Make visualization of predicted cells
+    pheno_channels = [pheno_map[..., i] for i in range(pheno_map.shape[2])]
+    pheno_vis = mix(panel_colors, pheno_channels)
+    draw_cell_coordinates(pheno_vis, cell_centers, vis_radius)
+    save_as_png(pheno_vis, output_path / "phenotype_prediction.png")
+
+    # Output 2:
+    # Visualise prediction on a provided image
+    if display_image_path is not None:
+        if display_image_path.suffix in [".tiff", ".tif"]:
+            display_image = tifffile.imread(display_image_path)
+        else:
+            display_image = imageio.imread(display_image_path)
+
+        draw_cell_coordinates(display_image, cell_centers, vis_radius)
+
+        im = Image.fromarray(display_image.astype("uint8"))
+        im.type = "I"
+        im.save(output_path / "prediction_vis.jpg", "JPEG", quality=90)
 
     prediction_path = output_path / "prediction.tsv"
     # Add pseudo-channels for phenotype identification
@@ -159,6 +199,13 @@ def demo(argv):
         required=False,
         help="a path to save output of demo inference")
     parser.add_argument(
+        "--display_image_path",
+        type=str,
+        default=None,
+        required=False,
+        help="a path to a file to use to display the detected cells",
+    )
+    parser.add_argument(
         "--log_th",
         type=float,
         default=0.07,
@@ -179,16 +226,27 @@ def demo(argv):
         required=False,
         help="the maximum standard deviation for Gaussian kernel of LoG, increase to detect larger cells",
     )
+    parser.add_argument(
+        "--vis_radius",
+        type=int,
+        default=2,
+        required=False,
+        help="the maximum standard deviation for Gaussian kernel of LoG, increase to detect larger cells",
+    )
 
     args = parser.parse_args(argv)
     tile_path = Path(args.tile_path)
     model_path = Path(args.model_path)
     output_path = Path(args.output_path)
+    display_image_path = (
+        Path(args.display_image_path) if args.display_image_path is not None else None
+    )
     log_th = args.log_th
     min_log_std = args.min_log_std
     max_log_std = args.max_log_std
+    vis_radius = args.vis_radius
 
-    demo_inference(tile_path, model_path, output_path, log_th, min_log_std, max_log_std)
+    demo_inference(tile_path, model_path, output_path, display_image_path, log_th, min_log_std, max_log_std, vis_radius)
 
 
 if __name__ == "__main__":
